4d0b40db9e5f7372e29ed136dc907a57c73b2e93
aneeskc <anees.kc01@gmail.com>
2016-01-24 03:16:14 -0800

    no message

--- application/views/home.php
+++ application/views/home.php
@@ -1,10 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-	<meta charset="UTF-8">
-	<title>home</title>
+<style>
+#abc
+{
+width: 100%;
+height: 90%;
+background:white;
+}
+#picture
+{
+float:left;
+width:37.8%;
+height:71.8%;
+background:white;
+margin:0% 0% 0% 3.5%;
+border:5px black solid;
+}
+#content
+{
+float:right;
+width:42%;
+height:72.5%;
+margin:0% 5% 0% 0%;
+background:white;
+background:url(10.jpg);
+font-size:19px;
+}
+#heading
+{
+fontsize:50pt;
+width:100%;
+height:20%;
+background:white;
+}
+#footer
+{
+width:100%;
+height:8%;
+background:#8FBC8F;
+margin:0%0%0%0%;
+colour:white;
+}
+
+</style>
 </head>
 <body>
-	welcome codeigniter
+<div id="heading">
+<table align="center" width="100%">
+<tr>
+<td><img src="pic4.jpg"></td>
+<td><img src="images/heading.png"></td>
+</tr>
+</table>
+</div>
+<div id="abc">
+<table align="center" width="80%" style="text-decoration:none;">
+<tr>
+<td><a href="home.html"style="text-decoration:none;"><img src="<?php echo base_url('images/home.png');?>"></a></td>
+<td><a href="register.html"style="text-decoration:none;"><img src="<?php echo  base_url('images/register.png');?>"></a></td>
+<td><a href="gallery.html"style="text-decoration:none;"><img src="<?php echo  base_url('images/gallery.png');?>"></a></td>
+<td><a href="hospitals.html"style="text-decoration:none;"><img src="<?php echo  base_url('images/hospitals.png');?>"></a></td>
+<td><a href="donors.html"style="text-decoration:none;"><img src=" <?php echo base_url('images/donors.png');?>"></a></td>
+<td><a href="aboutus.html"style="text-decoration:none;"><img src=" <?php echo  base_url('images/aboutus.png');?>"></a></td>
+<td><a href="contactus.html"style="text-decoration:none;"><img src=" <?php echo  base_url('images/contact.png');?>"></a></td>
+</tr>
+</table>
+<br/><br/><br/><div id="picture"><img src="anees.gif"></div>
+<div id="content"><i><b><center>
+<p style="margin-top:120px;">We Cherish Our New <br/>
+Lives! Because Of<br/>
+Your Generousity &<br/>
+Thoughfulness- We<br/>
+can Breath Freely,<br/>
+Live Life with New <br/>
+Lungs, a Heart, A<br/>
+Liver and So much<br/>
+More.........</p></center></i></b></div></div>
+<div id="footer"><marquee><b><i><br/>Death is the Begnning of new Life</i></b></marquee></div>
 </body>
 </html>
